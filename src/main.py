cb86d44601f1e076d9c66b7a99143efeec3df7d0
Luke Kozak <kozakluke@gmail.com>
2018-02-25 15:48:58 +0100

    Update main, add create_version, upload_version

--- src/main.py
+++ src/main.py
@@ -1,14 +1,29 @@
 import shotgun_api3
+import json
+import glob
 from pprint import pprint
+import os
+
+sg = shotgun_api3.Shotgun('https://juicewro.shotgunstudio.com',
+                          'job-version-daemon',
+                          '3819096b36111394a58a2d7280059e1951eafcaba663b53ba2fe546cd3cab6f7')
 
 
 def main():
-    sg = shotgun_api3.Shotgun('https://juicewro.shotgunstudio.com',
-                              'job-version-daemon',
-                              '3819096b36111394a58a2d7280059e1951eafcaba663b53ba2fe546cd3cab6f7')
+    update()
 
-    sg.config.sudo_as_login = 't.dyrdula'
-    data = {
+
+def update():
+    jobs = filter(os.path.isfile, glob.glob('/home/luke/Dropbox/S/*'))
+    for job in jobs:
+        data = json.load(open(job, 'r'))
+        version = create_version(data)
+        upload_version(version)
+
+
+def create_version(data):
+    sg.config.sudo_as_login = data['userName']
+    datax = {
         'code': 'Test.mp4',
         'project': {
             'type': 'Project',
@@ -18,25 +33,11 @@ def main():
         'updated_by': {'type': 'HumanUser', 'id': 87},
         'sg_path_to_movie': '../Test.mp4',
     }
+    return sg.create('Version', datax)
 
-    version = sg.create('Version', data)
 
-    sg.config.sudo_as_login = 'kozak'
-    data = {
-        'code': 'Test.mp4',
-        'project': {
-            'type': 'Project',
-            'id': 127
-        },
-        'user': {'type': 'HumanUser', 'id': 87},
-        'updated_by': {'type': 'HumanUser', 'id': 87},
-        'sg_path_to_movie': '../Test.mp4',
-    }
-
-    version = sg.create('Version', data)
-    #sg.upload('Version', version['id'], '../Test.mp4', 'sg_uploaded_movie')
-    pprint( version )
-    pprint(sg.config)
+def upload_version(version):
+    sg.upload('Version', version['id'], '../Test.mp4', 'sg_uploaded_movie')
 
 
 if __name__ == "__main__":
