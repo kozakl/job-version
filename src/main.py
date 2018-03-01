7c371876cb07de7e9040f098fdf29c312a7a101d
Luke Kozak <kozakluke@gmail.com>
2018-03-01 16:42:47 +0100

    Update main, pass dynamic data to create_version

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import glob
 from pprint import pprint
 import os
 import sys
+import os.path
 
 sg = shotgun_api3.Shotgun('https://juicewro.shotgunstudio.com',
                           'job-version-daemon',
@@ -12,38 +13,38 @@ sg = shotgun_api3.Shotgun('https://juicewro.shotgunstudio.com',
 
 
 def main():
+    update()
+    return
     while True:
         update()
         time.sleep(15)
 
 
 def update():
-    print('Update')
-
     jobs = filter(os.path.isfile, glob.glob('S:/jobs/*'))
     for job in jobs:
         data = json.load(open(job, 'r'))
-        version = create_version(data)
-        print upload_version(version)
+        if os.path.isfile(data['movie']):
+            version = create_version(data)
+            upload_version(version)
+        #print( data['projectId'] )
 
 
-def create_version(data):
-    sg.config.sudo_as_login = data['userName']
-    datax = {
-        'code': 'Test.mp4',
+def create_version(job_data):
+    sg.config.sudo_as_login = job_data['userName']
+    data = {
+        'code': os.path.basename(job_data['movie']),
         'project': {
             'type': 'Project',
-            'id': 127
+            'id': job_data['projectId']
         },
-        'user': {'type': 'HumanUser', 'id': 87},
-        'updated_by': {'type': 'HumanUser', 'id': 87},
-        'sg_path_to_movie': 'W:/0000_sg_development/render/Draft/shotgun_h264.mov',
+        'sg_path_to_movie': job_data['movie'],
     }
-    return sg.create('Version', datax)
+    return sg.create('Version', data)
 
 
 def upload_version(version):
-    sg.upload('Version', version['id'], 'W:/0000_sg_development/render/Draft/shotgun_h264.mov', 'sg_uploaded_movie')
+    sg.upload('Version', version['id'], version['sg_path_to_movie'], 'sg_uploaded_movie')
 
 
 if __name__ == "__main__":
